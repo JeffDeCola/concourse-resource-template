9747e902c941cd8c5c6f9957563b284527b4bea1
Jeff DeCola (TK1-PC Bash on Ubuntu on Windows) <jeffdecola@gmail.com>
2018-08-30 17:47:59 -0700

    udpated concourse

--- update_concourse.sh
+++ update_concourse.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # resource-template update_concourse.sh
 
-fly -t ci set-pipeline -p resource-template -c ci/pipeline.yml --load-vars-from ci/.credentials.yml
+fly -t ci set-pipeline -p resource-template -c ci/pipeline.yml --load-vars-from ../.credentials.yml
